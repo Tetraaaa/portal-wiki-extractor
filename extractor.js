6673bb04f8fa0552326ffdb2ecd55c68f3b17035
Tetra <nicolas.strohl@gmail.com>
2025-04-06 13:45:50 +0200

    Skip de tous les extraits audio de potatOS

--- extractor.js
+++ extractor.js
@@ -1,54 +1,77 @@
 import fs from "node:fs";
 import { URL } from "node:url";
 
-const res = await fetch("https://theportalwiki.com/wiki/GLaDOS_voice_lines/fr");
+const url = "https://theportalwiki.com/wiki/GLaDOS_voice_lines/fr";
+
+const res = await fetch(url);
 const html = await res.text();
 const portalTwoRelatedStuff = html.split(`id="Portal_2"`).pop();
 
-let matches = [...portalTwoRelatedStuff.matchAll(/<li>.*<\/li>/g)];
+let matches = [
+  ...portalTwoRelatedStuff.matchAll(
+    /<li>.*<\/li>|<span class="mw-headline".*<\/span>/g
+  ),
+].map((match) => match[0]);
 
 let samples = [];
 
-matches.forEach((match) => {
-  let audioLink = match[0]
-  .split(`class="internal"`)[0]
-  .split(`href="`)
-  .pop()
-  .replaceAll(`"`, "")
-  .replace(" ", "")
+let shouldSkipCurrentMatch = false;
 
-  let text = match[0].split(`</a>`)[0].split(">").pop().replaceAll(`"`, "").replaceAll("&#160;", "").replaceAll(/\[.*\]/g, "")
-  if(text)
-  {
-    try {
-      samples.push({
-        audioLink,
-        text,
-      });
-    } catch (error) {
-      console.log(
-        "Error while getting proper values for match : ",
-        match,
-        ", skipping."
-      );
-    }
+for (let i = 0; i < matches.length; i++) {
+  const match = matches[i];
+  if (match.includes("mw-headline")) {
+    const id = match.split("id=").pop().split(">")[0].replaceAll(`"`, "");
+    //Skip the parts where GLaDOS is a potato, because audio quality of those samples is purposely bad
+    if (id === `Chambre_de_GLaDOS_:_PoTATOS`) shouldSkipCurrentMatch = true;
+    if (id === `L'espace`) shouldSkipCurrentMatch = false;
+    continue;
   }
-});
 
+  if (shouldSkipCurrentMatch) continue;
+
+  let audioLink = match
+    .split(`class="internal"`)[0]
+    .split(`href="`)
+    .pop()
+    .replaceAll(`"`, "")
+    .replace(" ", "");
+
+  let text = match
+    .split(`</a>`)[0]
+    .split(">")
+    .pop()
+    .replaceAll(`"`, "")
+    .replaceAll("&#160;", "")
+    .replaceAll(/\[.*\]/g, "")
+    .trim();
+
+  if (!text || !audioLink) continue;
+
+  if(text === "N'espérez surtout pas revenir.") text = "Ne comptez surtout pas revenir."
+  if(text === "OK.") text = "Bien."
+  if(text === "Assez !") text = "Arrêtez !"
+  if(text === "Salle de test terminée. Dans l'intérêt de la science, le centre d'enrichissement est fier de vous présenter la liste de nombres suivante : neuf. Sept. Cinquante. Trois. Sept cent sept.") text = "Vous vous débrouillez très bien tous les deux."
+  if(text === "Le centre d'enrichissement va maintenant vous communiquer une liste de nombres et de fruits. Prenez soin de les noter car ils auront de l'importance ultérieurement dans l'expérience. Pas les fruits, toutefois. Sept. Avocat. Quarante. Veuillez rejoindre la salle de test suivante.") text = "Vous vous débrouillez très bien tous les deux."
+  if(text === "Salle de test terminée. Dans l'intérêt de -  Cent sept.") text = "J'ai fait un peu de lecture. Saviez-vous que le mot Orange a la même racine latine que le mot traitre ?"
+  if(text === "Excellent.") text = "Parfait."
+
+  samples.push({
+    audioLink,
+    text,
+  });
+}
+
+fs.mkdirSync("./wav")
 fs.writeFileSync(`./metadata.csv`, "");
 
 for (let i = 0; i < samples.length; i++) {
   const element = samples[i];
-  if(!element.audioLink) {
-    console.log("Element had invalid values, skipping", element);
-    continue;
-  }
 
   try {
     let parsedUrl = new URL(element.audioLink);
   } catch (_) {
     console.log("Got invalid url for element :", element.audioLink, ", skipping");
-    continue;  
+    continue;
   }
 
   // fs.writeFile(`./wav/${i}.txt`, element.text, () => {});
@@ -57,5 +80,5 @@ for (let i = 0; i < samples.length; i++) {
     .then((buf) => {
       fs.writeFile(`./wav/${i}.wav`, Buffer.from(buf), () => {});
     });
-    fs.appendFileSync(`./metadata.csv`, `\n${i}|"${element.text}"`);
+    fs.appendFileSync(`./metadata.csv`, `${i}|"${element.text}"\n`);
 }
